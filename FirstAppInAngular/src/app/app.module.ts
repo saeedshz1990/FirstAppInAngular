71f68370d7ac3e29c0e0df95449d35849d33540d
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-08-23 05:39:54 +0330

    create component with Angular Cli and Without use cli for create component

--- FirstAppInAngular/src/app/app.module.ts
+++ FirstAppInAngular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {UserComponent} from "./user/user.component";
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
---
+++ FirstAppInAngular/src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  // template:`<h1>This a Test For User Component</h1>`,
+  styleUrls: ['./user.component.css'],
+  // styles: [`h1 {
+  //     color : red;
+  // }`]
+})
+export class UserComponent {
+
+}
