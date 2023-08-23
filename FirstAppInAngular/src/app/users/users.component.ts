858acde84b6c5d55230ad3db359e03b2c5496f94
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-08-23 05:51:06 +0330

    define selectors

--- FirstAppInAngular/src/app/users/users.component.ts
+++ FirstAppInAngular/src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-users',
+  selector: '[app-users]',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
