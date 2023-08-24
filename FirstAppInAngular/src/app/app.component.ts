159cc06a5554c338d3da18d2ca54e25fb4c97e44
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-08-24 06:53:25 +0330

    sample databinding for practice

--- FirstAppInAngular/src/app/app.component.ts
+++ FirstAppInAngular/src/app/app.component.ts
@@ -8,10 +8,34 @@ import {Component} from '@angular/core';
 export class AppComponent {
   name: string = 'Saeed';
   isEnabled: boolean = false;
-
+  userName: string = '';
+  password: string = '';
+  retypePassword: string = '';
+  isPasswordMatch: boolean = false;
 
   OnInpitChange(e: Event) {
     this.name = (<HTMLInputElement>e.target).value;
     console.table(this.name);
+  }
+
+  onUserNameInputChanged(e: Event) {
+    this.userName = (<HTMLInputElement>e.target).value;
+    console.log(this.userName);
+  }
+
+  onPasswordInputChanged(e: Event) {
+    this.password = (<HTMLInputElement>e.target).value;
+  }
+
+  onRetypePasswordInputChanged(e: Event) {
+    this.retypePassword = (<HTMLInputElement>e.target).value;
+
+    // this.isPasswordMatch = this.password !== this.retypePassword ? false : true;
+
+    if (this.password !== this.retypePassword) {
+      this.isPasswordMatch = false;
+    } else {
+      this.isPasswordMatch = true;
+    }
   }
 }
