a226d820b725db41e851e6b245c8fff90371a93a
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-08-28 06:37:31 +0330

    lifecycleHooks part two implementing

--- FirstAppInAngular/src/app/users/users.component.ts
+++ FirstAppInAngular/src/app/users/users.component.ts
@@ -1,26 +1,72 @@
-import {AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef, Input, OnChanges, OnDestroy,
+  OnInit, SimpleChanges, ViewChild
+} from '@angular/core';
 
 @Component({
   selector: '[app-users]',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit, AfterContentInit, AfterContentChecked {
+export class UsersComponent implements OnInit,
+  AfterContentInit, AfterContentChecked, DoCheck, OnChanges, AfterViewInit, AfterViewChecked, OnDestroy {
+  @ViewChild('par2', {static: true})
+  par2!: ElementRef;
   @ContentChild('par', {static: true})
   par!: ElementRef;
+// @ts-ignore
+  @Input() var1;
 
+  constructor() {
+  }
 
   ngOnInit(): void {
-    console.log("Ng On Init" + this.par.nativeElement.textContent)
+    // console.log("Ng On Init" + this.par.nativeElement.textContent)
+    console.log("ngOnInit Called")
   }
 
   ngAfterContentChecked(): void {
-    console.log("Ng AfterContentChecked" + this.par.nativeElement.textContent)
+    // console.log("Ng AfterContentChecked" + this.par.nativeElement.textContent)
+    console.log("ngAfterContentChecked Called")
 
   }
 
   ngAfterContentInit(): void {
-    console.log("Ng AfterContentInit" + this.par.nativeElement.textContent)
+    // console.log("Ng AfterContentInit" + this.par.nativeElement.textContent)
+    console.log("ngAfterContentInit Called")
 
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked Called")
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit Called")
+
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck Called")
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges Called")
+
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy Called")
+
+  }
+  onChangeViewChild(){
+    this.par2.nativeElement.innerHTML="View Child Changed"
   }
 }
